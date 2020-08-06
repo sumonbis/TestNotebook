9ad74c5381b61451854eed08e48b0508ecd3c99e
Sumon Biswas <39540376+sumonbis@users.noreply.github.com>
2020-08-06 15:59:44 -0500

    a

--- test1.py
+++ test1.py
@@ -1,1 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn import datasets
+
 iris = datasets.load_iris()
+X = iris.data[:, 0:2]  # we only take the first two features for visualization
+y = iris.target
+
+n_features = X.shape[1]
+
+C = 10
+kernel = 1.0 * RBF([1.0, 1.0])  # for GPC
